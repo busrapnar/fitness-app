d0fc836b1732f2e3e0f7adbb5712a687e655659a
busrapinar <bsrapnrr@gmail.com>
2024-01-23 23:43:34 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Navbar from "./scenes/navbar"
+
+function App() {
+  return (
+    <>
+      <div className=" bg-gray-20">
+       <Navbar/>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
